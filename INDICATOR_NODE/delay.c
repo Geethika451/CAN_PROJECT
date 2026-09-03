2a85dea4f26cef48e3dc0a9f9d1243e7bb67149d
Geethika451 <edurugeethika@gmail.com>
2026-09-03 10:23:52 +0530

    Add files via upload

---
+++ INDICATOR_NODE/delay.c
@@ -0,0 +1,44 @@
+#include "types.h"
+
+
+void delay_us(u32 dlyUS)
+
+{
+
+	dlyUS*=12;
+
+	while(dlyUS--);
+
+}
+
+
+
+
+void delay_s(unsigned int dlyMS)
+
+{
+
+  int i;
+
+  for(;dlyMS>0;dlyMS--)
+
+  {
+
+		for(i=12000000;i>0;i--);
+
+  }	
+  }	
+void delay_ms(unsigned int dlyMS)
+
+{
+
+  int i;
+
+  for(;dlyMS>0;dlyMS--)
+
+  {
+
+		for(i=12000;i>0;i--);
+
+  }		
+}
---
+++ INDICATOR_NODE/indicator.c
@@ -0,0 +1,44 @@
+#include<lpc21xx.h>
+#include"types.h"
+#include "lcd_defines.h"
+#include "can.h"
+#include "can_defines.h"
+#include "delay.h"
+#include "interrupt.h"
+
+struct CAN_Frame rxFrame;
+
+int main()
+{
+s32 i;
+//InitLcd();
+Init_CAN1();
+IODIR0|=255<<0;
+IOSET0=255<<0;
+while(1)
+{
+   CAN1_Rx(&rxFrame);
+	 if(rxFrame.ID==IND_ID)
+	 {
+  if(rxFrame.Data1=='L')
+
+  {
+  for(i=7;i>=0;i--)
+  {
+  IOCLR0=1<<i;
+   delay_ms(150);
+   IOSET0=1<<i;
+  }
+  }
+  else if(rxFrame.Data1=='R')
+  {	
+ for(i=0;i<8;i++)
+ {
+  IOCLR0=1<<i;
+  delay_ms(150);
+  IOSET0=1<<i;
+  }
+  }
+  }
+  }
+  }
